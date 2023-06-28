082280c8c0b84ec319749f881a39bc61c6ebf722
gigastp <109027241+gigastp@users.noreply.github.com>
2023-06-28 23:02:46 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+
 TIMEREGION=Europe;
 TIMECITY=Kiev;
 EDITOR=vi;
+NETWORKMANAGER=dhcpcd;
 
 function set_locale() {
     echo "Uncomment en_US.UTF-8 UTF-8 and other needed locales";
@@ -12,23 +15,47 @@ function set_locale() {
 function create_user() {
     echo -n "Enter hostname: "; read PCNAME;
     echo -n "Enter username: "; read USERNAME;
-
+    echo ${PCNAME} > /etc/hostname
     useradd -m -s /bin/bash ${USERNAME} || return $?;
+    
     passwd ${USERNAME};
     while (( $? )); do
         echo -e "\nTry again:"; passwd ${USERNAME};
     done
-
+    
     TMPFILE=`mktemp`; echo -e "${USERNAME} ALL=(ALL:ALL) ALL\n" > ${TMPFILE};
+    cat ${TMPFILE} /etc/fstab; echo -e "User appended to sudoers.";
+}
+
+function install_bootloader() {
+    ls /sys/firmware/efi/efivars > /dev/null;
+    if (( $? )); then
+        echo -n "Boot dev: "; read BOOTDEV;
+        
+        grub-install --target=i386-pc /dev/${BOOTDEV} || return $?;
+        while (( $? )); do
+            echo -ne "Try Again:\nBoot dev: "; read BOOTDEV;
+            grub-install --target=i386-pc /dev/${BOOTDEV};            
+        done
+    else
+        grub-install --target=x86_64-efi --efi-directory=/mnt/boot --bootloader-id=GRUB || return $?;
+    fi
+    
+    grub-mkconfig -o /boot/grub/grub.cfg || return $?;
 }
 
 function install() {
     # Timezone
     ln -sf /usr/share/zoneinfo/${TIMEREGION}/${TIMECITY} /etc/localtime \
-    && hwclock --systohc || return $?; echo -e "\nTimezon: setup complated.";
-    
-    set_locale || return $?; echo -e "\nLocale configured.";
-    create_user || return $?; echo -e "\nUser created.";
+    && hwclock --systohc || return $?; echo -e "Timezone setup complated.\n";
+
+    set_locale || return $?; echo -e "Locale configured.\n"; 
+    create_user || return $?; echo -e "User created.\n";
+    install_bootloader || return $?; echo -e "Bootloader installed.\n";
+
+    # Network
+    systemctl enable ${NETWORKMANAGER}.service && ufw enable || return $? \
+    && echo -e "Network configured.\n";
 }
 
 install
