e70bedeedcd9bbf3df20188fab4f221bc071d882
gigastp <109027241+gigastp@users.noreply.github.com>
2023-06-29 01:17:53 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,23 +18,6 @@ function mount_partiotions() {
     fi
 }
 
-function install_bootloader() {
-    ls /sys/firmware/efi/efivars &> /dev/null;
-    if (( $? )); then
-        echo -n "Boot dev: "; read BOOTDEV;
-        
-        grub-install --target=i386-pc /dev/${BOOTDEV};
-        while (( $? )); do
-            echo -e "\nTry Again:\nBoot dev: "; read BOOTDEV;
-            grub-install --target=i386-pc /dev/${BOOTDEV};            
-        done
-    else
-        grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB || return $?;
-    fi
-    
-    grub-mkconfig -o /boot/grub/grub.cfg || return $?;
-}
-
 function install() {
     mount_partiotions
     while (( $? )); do
@@ -44,9 +27,8 @@ function install() {
     pacstrap -K /mnt base linux linux-firmware ${EXTRAPACKAGES} || return $?;
     echo -e "System setup completed.";
     genfstab -U /mnt > /mnt/etc/fstab || return $?; echo -e "/etc/fstab created.";
-    install_bootloader || return $?; echo -e "Bootloader installed.\n";    
 
-    echo -e "Entering in chroot..."; arch-chroot /mnt || return $?;
+    echo -e "Entering in chroot...\n"; arch-chroot /mnt || return $?;
 }
 
 install
