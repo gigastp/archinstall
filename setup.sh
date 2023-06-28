64ceca0171619421dba61a845d1173a6bd680aa7
gigastp <109027241+gigastp@users.noreply.github.com>
2023-06-28 23:13:39 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -45,7 +45,7 @@ function install_bootloader() {
             grub-install --target=i386-pc /dev/${BOOTDEV};            
         done
     else
-        grub-install --target=x86_64-efi --efi-directory=/mnt/boot --bootloader-id=GRUB || return $?;
+        grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB || return $?;
     fi
     
     grub-mkconfig -o /boot/grub/grub.cfg || return $?;
