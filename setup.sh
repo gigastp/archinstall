bb2e094d9b0e2864464d1491b627fc61fdb13338
gigastp <109027241+gigastp@users.noreply.github.com>
2023-06-29 22:33:58 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,7 +22,7 @@ function create_user() {
     # Probably bug in useradd or pwd (when arg of option -b ended with /):
     # output of pwd after user creation: /home//<username>
     # Note: after `cd ~` pwd showed correct path
-    useradd -m -s /bin/bash -b /home "${USERNAME}" || return $?;
+    useradd -m -s /bin/bash "${USERNAME}" || return $?;
 
     echo "Set root password:"; passwd;
     while (( $? )); do
